dbaab9fbe33146ecbe17bcf8e9daf17595512f2f
MrunmayeeL <mrunmayee.m.limaye@gmail.com>
2025-07-16 21:00:21 +0530

    Add files via upload

---
+++ python_scripts/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+import psycopg2
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+conn = psycopg2.connect(
+    dbname="northwind",
+    user="postgres",         
+    password="Mrunmayee", 
+    host="localhost",
+    port="5432"
+)
